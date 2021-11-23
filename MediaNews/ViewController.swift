91dae04eae761df523117dd199a25ead828f474b
itsXell <menh.keo@gmail.com>
2021-11-23 20:38:39 +0700

    Initial commit

---
+++ MediaNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaNews
+//
+//  Created by Xell on 23/11/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MediaNews' do
+   pod 'RxSwift'
+   pod 'RxCocoa'
+   pod 'Presentr'
+end
